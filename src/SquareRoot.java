effe6382228bc0bd4ae0a18f5a428e6115c65e4c
Vedant Thakur <vedant.thakur.btech2023@sitpune.edu.in>
2025-04-12 23:12:20 +0530

    Updating the Custom Exception for Negative Numbers

--- src/SquareRoot.java
+++ src/SquareRoot.java
@@ -1,7 +1,7 @@
 public class SquareRoot {
     public static double squareRoot(double a) {
         if (a<0){
-            throw new ArithmeticException("SquareRoot called with a negative value");
+            throw new NegativeInputException("Cannot Calculate Square Root of a negative value");
         }
         return Math.sqrt(a);
     }
