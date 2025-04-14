ce59472a4856b48c8a5d5d9b88797a0bcbb21a1d
Vedant Thakur <vedant.thakur.btech2023@sitpune.edu.in>
2025-04-14 12:53:30 +0530

    Adding Comments to  NegativeInputException for better Readibility

--- src/NegativeInputException.java
+++ src/NegativeInputException.java
@@ -1,6 +1,7 @@
+// Custom exception class for handling negative inputs in square root
 public class NegativeInputException extends RuntimeException {
     public NegativeInputException(String message) {
 
-        super(message);
+        super(message); // Pass message to Exception superclass
     }
 }
