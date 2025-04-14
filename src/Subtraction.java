01dc86aaf08cad27b7cbc305abe34c008bad7d0d
Vedant Thakur <vedant.thakur.btech2023@sitpune.edu.in>
2025-04-14 12:56:51 +0530

    Adding Comments Square Function for better Readibility

--- src/Subtraction.java
+++ src/Subtraction.java
@@ -1,4 +1,6 @@
+// Class to perform subtraction operation
 public class Subtraction {
+    // Static method that returns the difference of two integers
     public static int subtract(int a, int b) {
         return a - b;
     }
