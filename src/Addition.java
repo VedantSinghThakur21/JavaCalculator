3893b2271a7954ce3ac8c65b89e4671e5bc0204a
Vedant Thakur <vedant.thakur.btech2023@sitpune.edu.in>
2025-04-14 12:44:33 +0530

    Adding Comments to Addition Function for better Readibility

--- src/Addition.java
+++ src/Addition.java
@@ -1,4 +1,6 @@
+// Class to perform addition operation
 public class Addition {
+    // Static method that returns the sum of two integers
     public static int add(int a, int b) {
         return a + b;
     }
