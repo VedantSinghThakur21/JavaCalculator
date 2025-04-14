267c2ca3721ca58c63a1ffd5d23344d1b529cf4a
Vedant Thakur <vedant.thakur.btech2023@sitpune.edu.in>
2025-04-14 12:46:16 +0530

    Adding Comments to Division Function for better Readibility

--- src/Division.java
+++ src/Division.java
@@ -1,6 +1,9 @@
+// Class to perform division operation
 public class Division {
+    // Static method to divide two integers and return the result as double
     public static double divide(double a, double b) {
         if (b==0){
+            // Throw arithmetic exception if denominator is zero
             throw new ArithmeticException("Divide by zero");
         }
         return (double) a / b;
