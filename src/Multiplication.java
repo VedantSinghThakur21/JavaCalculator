16bfc8cd9c0f65deb4bfc2757d57781b8b8700f6
Vedant Thakur <vedant.thakur.btech2023@sitpune.edu.in>
2025-04-14 12:51:13 +0530

    Adding Comments Multiplication Function for better Readibility

--- src/Multiplication.java
+++ src/Multiplication.java
@@ -1,4 +1,6 @@
+// Class to perform multiplication operation
 public class Multiplication {
+    // Static method that returns the product of two integers
     public static int multiply(int a, int b) {
         return a * b;
     }
