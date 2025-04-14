00d9e7103ee2ae704dafb55b6dbc5ba5799ae5c0
Vedant Thakur <vedant.thakur.btech2023@sitpune.edu.in>
2025-04-14 12:48:25 +0530

    Adding Comments to InvalidMenuChoice Exception for better Readibility

--- src/InvalidMenuChoiceException.java
+++ src/InvalidMenuChoiceException.java
@@ -1,5 +1,6 @@
+// Custom exception class for invalid choices in calculator menu
 public class InvalidMenuChoiceException extends RuntimeException {
     public InvalidMenuChoiceException(String message) {
-        super(message);
+        super(message); // Pass message to Exception superclass
     }
 }
