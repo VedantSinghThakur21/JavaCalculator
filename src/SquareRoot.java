1709e994452377791ba7d99b65628065ce61e5e5
Vedant Thakur <vedant.thakur.btech2023@sitpune.edu.in>
2025-04-14 12:55:44 +0530

    Adding Comments SquareRoot Function for better Readibility

--- src/SquareRoot.java
+++ src/SquareRoot.java
@@ -1,4 +1,6 @@
+// Class to calculate square root of a number
 public class SquareRoot {
+    // Throws a custom exception if input is negative
     public static double squareRoot(double a) {
         if (a<0){
             throw new NegativeInputException("Cannot Calculate Square Root of a negative value");
